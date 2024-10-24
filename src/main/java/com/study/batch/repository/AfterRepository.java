95beed9437e5c7c303a9ff764b456dd16564a0d1
ironshine <cjfgml2320@gmail.com>
2024-10-24 10:58:25 +0900

    init

---
+++ src/main/java/com/study/batch/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package com.study.batch.repository;
+
+import com.study.batch.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
